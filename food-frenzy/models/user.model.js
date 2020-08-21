08c4a162acfba62403c742b438202960d97ce59c
Andre Urcia <aurcia@c20g.com>
2020-08-21 16:22:14 -0400

    Added fields to user schema.

--- food-frenzy/models/user.model.js
+++ food-frenzy/models/user.model.js
@@ -6,6 +6,18 @@ const schema = new Schema({
   hash: { type: String, required: true },
   firstName: { type: String, required: false },
   lastName: { type: String, required: false },
+  bumpable: { type: Boolean, required: true },
+  address: { type: String, required: true },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+  zipcode: { type: String, required: true },
+  contact: { type: String, required: false },
+  allergies: { type: String[], required: false },
+  bump_limit: { type: Number, required: false },
+  bump_period: { type: String, required: false },
+  buyable: { type: Boolean, required: true },
+  buy_limit: { type: Number, required: false },
+  buy_period: { type: String, required: false },
 });
 
 schema.set("toJSON", { virtuals: true });

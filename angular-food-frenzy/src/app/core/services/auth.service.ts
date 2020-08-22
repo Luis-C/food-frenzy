4a0b4e31ffce584f4e90340551298a6e5a9788db
Luis Cabrera <ziul.ch@gmail.com>
2020-08-21 22:47:07 -0400

    Working on AuthService
    Not tested

---
+++ angular-food-frenzy/src/app/core/services/auth.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from '../models/user';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
+  private PATH = 'http://localhost:3030/';
+
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<User>(
+      JSON.parse(localStorage.getItem('currentUser'))
+    );
+
+    // currentUser is turned into an Observable that will allow other parts of the app to subscribe and get notified when currentUserSubject changes.
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue(): User {
+    return this.currentUserSubject.value;
+  }
+
+  signIn(username: string, password: string): Observable<any> {
+    // Changed to network based authentication strategy.
+
+    // Read more here: https://angular.io/guide/http
+    return this.http
+      .post<any>(`${this.PATH}user/authenticate`, {
+        username,
+        password,
+      })
+      .pipe(
+        map((user) => {
+          // login successful if there's a jwt token in the response
+          if (user && user.token) {
+            localStorage.setItem('currentUser', JSON.stringify(user));
+            this.currentUserSubject.next(user);
+          }
+          return user;
+        })
+      );
+  }
+
+  signUp(user) {
+    return this.http.post(`${this.PATH}/user/register`, user);
+  }
+}

57899703fdd52edaa4bdebb6eb05addc6f358f47
Luis Cabrera <ziul.ch@gmail.com>
2020-08-21 22:19:04 -0400

    Updated user Model on Angular

--- angular-food-frenzy/src/app/core/models/user.ts
+++ angular-food-frenzy/src/app/core/models/user.ts
@@ -1,5 +1,50 @@
 export class User {
-  firstName: string;
-  email: string;
-  // TODO: define properties
+  username: string;
+  // we don't want the hash on the front end for security reasons.
+  bumpable: boolean;
+  buyable: boolean;
+
+  address: string;
+  city: string;
+  state: string;
+  zipcode: string;
+
+  // Optional parameters have ? in Typescript and go at the end of the object
+  // definition
+  contact?: string;
+  allergies?: string;
+  bump_limit?: number;
+  bump_period?: string;
+  buy_period?: string;
+
+  firstName?: string;
+  lastName?: string;
+
+  friends?: string[];
+  requests?: string[];
+  history?: JSON;
 }
+
+// const schema = new Schema({
+//   username: { type: String, unique: true, required: true },
+//   hash: { type: String, required: true },
+//   firstName: { type: String, required: false },
+//   lastName: { type: String, required: false },
+
+//   bumpable: { type: Boolean, required: true },
+//   address: { type: String, required: true },
+//   city: { type: String, required: true },
+//   state: { type: String, required: true },
+//   zipcode: { type: String, required: true },
+//   contact: { type: String, required: false },
+//   allergies: [{ type: String, required: false }],
+//   bump_limit: { type: Number, required: false },
+//   bump_period: { type: String, required: false },
+//   buyable: { type: Boolean, required: true },
+//   buy_limit: { type: Number, required: false },
+//   buy_period: { type: String, required: false },
+
+//   friends: [{ type: String, required: false }],
+//   requests: { type: String, required: false },
+//   history: { type: JSON, required: false },
+// });
